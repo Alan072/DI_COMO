15a502291a133ce03e18acd9b55360c790e1718c
Alan072 <91845623+Alan072@users.noreply.github.com>
2023-04-19 00:05:11 -0600

    Plantilla

---
+++ app/Http/Controllers/ControladorPaginas.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorPaginas extends Controller
+{
+    //
+
+    public function fhome()
+    {
+     return view('home');
+    }
+}
