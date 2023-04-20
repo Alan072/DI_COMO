c61090ec128ed32eb269f27175c313e0da53cb8d
Alexrex12 <alexrex192002@gmail.com>
2023-04-20 11:17:15 -0600

    tercer modificacion

--- resources/views/tareas.blade.php
+++ resources/views/tareas.blade.php
@@ -10,8 +10,72 @@
                     <textarea name="descripcion" style="resize: none;" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="XXXXXXXX" required></textarea>
                 </div>
                 <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID Salida</label>
-                    <input type="number" name="idsalida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required>
+                    <label for="nombre_producto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione la salida</label>
+                <select id="idsalida" name="idsalida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <option value="" disabled selected>Selecciona la salida</option>
+                    <?php
+                    // Realizar la conexión a la base de datos
+                    $conexion = mysqli_connect("localhost:3306", "root", "", "laravel");
+                
+                    // Verificar la conexión
+                    if (!$conexion) {
+                        die("Error al conectar a la base de datos: " . mysqli_connect_error());
+                    }
+                
+                    // Realizar la consulta a la base de datos
+                    $query = "SELECT * FROM salida";
+
+                    $resultado = mysqli_query($conexion, $query);
+                
+                    // Verificar si la consulta devolvió resultados
+                    if (mysqli_num_rows($resultado) > 0) {
+                        // Iterar sobre los resultados y crear los elementos de la lista desplegable
+                        while ($fila = mysqli_fetch_assoc($resultado)) {
+                        echo "<option value='" . $fila["id_salida"] . "'>" . $fila["id_salida"] . "</option>";
+                        }
+                    } else {
+                        // Si la consulta no devolvió resultados, mostrar un mensaje de error
+                        echo "<option value=''>No se encontraron resultados</option>";
+                    }
+                
+                    // Cerrar la conexión a la base de datos
+                    mysqli_close($conexion);
+                    ?>
+                </select>
+                </div>
+                <div>
+                    <label for="nombre_producto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione el Empleado</label>
+                <select id="idusuario" name="idusuario" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <option value="" disabled selected>Selecciona el Empleado</option>
+                    <?php
+                    // Realizar la conexión a la base de datos
+                    $conexion = mysqli_connect("localhost:3306", "root", "", "laravel");
+                
+                    // Verificar la conexión
+                    if (!$conexion) {
+                        die("Error al conectar a la base de datos: " . mysqli_connect_error());
+                    }
+                
+                    // Realizar la consulta a la base de datos
+                    $query = "SELECT * FROM tipo_usuario";
+
+                    $resultado = mysqli_query($conexion, $query);
+                
+                    // Verificar si la consulta devolvió resultados
+                    if (mysqli_num_rows($resultado) > 0) {
+                        // Iterar sobre los resultados y crear los elementos de la lista desplegable
+                        while ($fila = mysqli_fetch_assoc($resultado)) {
+                        echo "<option value='" . $fila["id_usuario"] . "'>" . $fila["nombre"] . "'>" . $fila["apellido_paterno"] . "'>" . $fila["apellido_materno"] . "</option>";
+                        }
+                    } else {
+                        // Si la consulta no devolvió resultados, mostrar un mensaje de error
+                        echo "<option value=''>No se encontraron resultados</option>";
+                    }
+                
+                    // Cerrar la conexión a la base de datos
+                    mysqli_close($conexion);
+                    ?>
+                </select>
                 </div>
                 <div>
                     <label for="nombre_producto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione el Empleado</label>
@@ -82,8 +146,38 @@
                     </select>
                 </div>
                 <div>
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ID Entrada</label>
-                    <input type="number" name="identrada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required>
+                    <label for="nombre_producto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione la entrada</label>
+                <select id="identrada" name="identrada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                    <option value="" disabled selected>Selecciona la entrada</option>
+                    <?php
+                    // Realizar la conexión a la base de datos
+                    $conexion = mysqli_connect("localhost:3306", "root", "", "laravel");
+                
+                    // Verificar la conexión
+                    if (!$conexion) {
+                        die("Error al conectar a la base de datos: " . mysqli_connect_error());
+                    }
+                
+                    // Realizar la consulta a la base de datos
+                    $query = "SELECT * FROM entrada";
+
+                    $resultado = mysqli_query($conexion, $query);
+                
+                    // Verificar si la consulta devolvió resultados
+                    if (mysqli_num_rows($resultado) > 0) {
+                        // Iterar sobre los resultados y crear los elementos de la lista desplegable
+                        while ($fila = mysqli_fetch_assoc($resultado)) {
+                        echo "<option value='" . $fila["id_entrada"] . "'>" . $fila["id_entrada"] . "</option>";
+                        }
+                    } else {
+                        // Si la consulta no devolvió resultados, mostrar un mensaje de error
+                        echo "<option value=''>No se encontraron resultados</option>";
+                    }
+                
+                    // Cerrar la conexión a la base de datos
+                    mysqli_close($conexion);
+                    ?>
+                </select>
                 </div>
             </div>
             <div class="flex">
@@ -99,4 +193,19 @@
       </form>
     </div>
 </div>
+<script>
+    const selectSalida = document.getElementById("idsalida");
+    const selectEntrada = document.getElementById("identrada");
+    
+    selectSalida.addEventListener("change", function() {
+        selectEntrada.disabled = true;
+        selectSalida.disabled = false;
+    });
+    
+    selectEntrada.addEventListener("change", function() {
+        selectSalida.disabled = true;
+        selectEntrada.disabled = false;
+    });
+</script>
+
 @stop
